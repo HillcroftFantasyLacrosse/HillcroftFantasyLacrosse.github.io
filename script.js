55606effe1e8cdd3686b790e06da2cded3fbdc2f
HillcroftFantasyLacrosse <hillcroftfantasylacrosse@gmail.com>
2016-09-14 11:01:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -312,7 +312,7 @@ function getPoints(data, sub) {
 }
  
 function getGameweekPoints(data, sub) {
-    var p = $("<p>");
+    var p = $(<p class="big">);
     p.append(data["gameweekpoints"]);
     return p;
 }
