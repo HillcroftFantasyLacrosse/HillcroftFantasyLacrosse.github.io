4d5cd463177b13aac4de51e661563eec75327b35
HillcroftFantasyLacrosse <hillcroftfantasylacrosse@gmail.com>
2016-09-14 09:30:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -230,8 +230,8 @@ $.getJSON("data/squads.json", function(squads) {
             $.each(manager["players"], function(undefined, namePlayer) {
                 var player = playerDict[namePlayer];
                 td.append(getPerson(player));
-                ti.append($("<td>").append(player["gameweekpoints"]);
-                ta.append($("<td>").append(player["points"]);                
+                ti.append($("<td>").append(player["gameweekpoints"]));
+                ta.append($("<td>").append(player["points"]));                
             });
             var tr = getRow(rank == 0 ? 1 : rank == managerList.length-1 ? -1 : 0);
             tr.append($("<td>").append(getPerson(manager)));
